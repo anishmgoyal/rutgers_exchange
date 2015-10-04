16de1e1fd163a9602897710e9a5f6423d5313666
PoorvaS <poorvasampat3@gmail.com>
2015-10-04 02:30:14 -0400

    product_controller method

--- backend/app/controllers/product_controller.rb
+++ backend/app/controllers/product_controller.rb
@@ -2,31 +2,79 @@ class ProductController < ApplicationController
 
   include SessionsHelper
     
-  before_filter :require_auth, only: [:read, :update, :logout]
+  before_filter :require_auth, only: [:read, :update, :remove]
 
   # PUT /product
   # Please see /outlines/product_api.txt
   def create
   end
-
-  # PUT /product
+ 
+  # GET /product
   # Please see /outlines/product_api.txt
-  def create
+  def list
+  end
+ 
+
+  # GET /product:id
+  # Please see /outlines/product_api.txt
+  def read
+    product = Product.find(params[:id])
+    if product
+      payload = {
+        product_name: product.product_name,
+        user: product.user.username,
+        type: product.type,
+        price: product.price,
+        status: product.sold_status
+      }
+    else
+      render status: 404, json: {error: true}
+    end
   end
 
-  # PUT /product
+  # POST /product:id
   # Please see /outlines/product_api.txt
-  def create
+  def update
+    product = Product.find(params[:id])
+    if product
+      if @current_user.id == product.user.id
+        if @current_product.update(params)
+          payload = {
+            error: false,
+            id: @current_product.id
+          }
+          render status: 200, json: payload
+        else
+          errors = []
+          product.errors.keys.each do |key|
+          errors << {field: key, message: product.errors.full_messages_for(key).first}
+        end
+        payload = {
+        error: true,
+        errors: errors
+        }
+        render status: 200, json: payload
+      else
+        render status: 403, json: {error: true}
+      end
+    else  
+      render status: 404, json: {error: true}
+    end
   end
 
-  # PUT /product
+  # DELETE /product:id
   # Please see /outlines/product_api.txt
-  def create
-  end
-
-  # PUT /product
-  # Please see /outlines/product_api.txt
-  def create
+  def delete
+    product = Product.find(param[:id])
+    if product
+      if @current_user.id == product.user.id
+          product.destroy
+      else
+        render status: 403, json: {error: true}
+      end
+    else
+      render status: 404, json: {error: true}
+    end    
   end
 
 end
