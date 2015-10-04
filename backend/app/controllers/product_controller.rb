a6af98b19b32850d0df1736704c51d498304c6ea
Anish Goyal <anishgoyal@outlook.com>
2015-10-04 03:27:54 -0400

    Merges for product, changes to application controller to disable inate csrf

--- backend/app/controllers/product_controller.rb
+++ backend/app/controllers/product_controller.rb
@@ -1,20 +1,34 @@
 class ProductController < ApplicationController
 
   include SessionsHelper
-    
-  before_filter :require_auth, only: [:read, :update, :remove]
+
+  before_filter :require_auth, only: [:create, :update, :delete]
 
   # PUT /product
   # Please see /outlines/product_api.txt
   def create
+    product = Product.new(params)
+    product.user = @current_user
+    if product.save()
+        payload = {
+            error: false,
+            id: product.id
+        }
+        render status: 200, json: product
+    else
+    
+    end
   end
- 
+
   # GET /product
   # Please see /outlines/product_api.txt
   def list
+    products = Product.all
+    products.each do |product|
+    
+    end
   end
  
-
   # GET /product:id
   # Please see /outlines/product_api.txt
   def read
@@ -22,59 +36,65 @@ class ProductController < ApplicationController
     if product
       payload = {
         product_name: product.product_name,
-        user: product.user.username,
+        user: {
+            user_id: product.user.user_id, username: product.user.username, first_name: product.user.first_name, last_name: product.user.last_name
+        },
         type: product.type,
         price: product.price,
         status: product.sold_status
       }
+      render status: 200, json: payload
     else
       render status: 404, json: {error: true}
     end
   end
 
-  # POST /product:id
-  # Please see /outlines/product_api.txt
-  def update
-    product = Product.find(params[:id])
-    if product
-      if @current_user.id == product.user.id
-        if @current_product.update(params)
-          payload = {
-            error: false,
-            id: @current_product.id
-          }
-          render status: 200, json: payload
+    # POST /product/:id
+    # Please see /outlines/product_api.txt
+    def update
+        product = Product.find(params[:id])
+        if product
+            if @current_user.id == product.user.id
+                if @current_product.update(params)
+                    payload = {
+                        error: false,
+                        id: @current_product.id
+                    }
+                    render status: 200, json: payload
+                else
+                    errors = []
+                    product.errors.keys.each do |key|
+                        errors << {field: key, message: product.errors.full_messages_for(key).first}
+                    end
+                    payload = {
+                        error: true,
+                        errors: errors
+                    }
+                    render status: 200, json: payload
+                end
+            else
+                render status: 403, json: {error: true}
+            end
         else
-          errors = []
-          product.errors.keys.each do |key|
-          errors << {field: key, message: product.errors.full_messages_for(key).first}
+            render status: 404, json: {error: true}
         end
-        payload = {
-        error: true,
-        errors: errors
-        }
-        render status: 200, json: payload
-      else
-        render status: 403, json: {error: true}
-      end
-    else  
-      render status: 404, json: {error: true}
     end
-  end
 
-  # DELETE /product:id
+  # DELETE /product/:id
   # Please see /outlines/product_api.txt
   def delete
     product = Product.find(param[:id])
     if product
       if @current_user.id == product.user.id
           product.destroy
+          render status: 200, json: {error: false}
       else
         render status: 403, json: {error: true}
       end
     else
       render status: 404, json: {error: true}
-    end    
+    end
   end
+
 
 end
--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+
+    # Accessor directives
+    attr_accessible :product_name, :type, :price
+
+    # Relations
+    belongs_to :user
+    
+    # Validation
+    PRICE_REGEX = /\A[0-9]{3,}\z/
+    SOLD_STATUS_REGEX = /\A(SOLD_NOT_SOLD|SOLD_IN_TRANSACTION|SOLD_SOLD)\z/
+    validates :product_name, presence: true
+    validates :type, presence: true
+    validates :price, presence: true, format: PRICE_REGEX
+    validates :sold_status, presence: true, format: SOLD_STATUS_REGEX
+
 end
