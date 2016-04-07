1a315a9f383d8606d8a61fba49e01964398acce0
Anish Goyal <anishgoyal@outlook.com>
2016-04-07 20:57:37 +0000

    Add info pages

---
+++ backend/app/assets/javascripts/pages/info.js
@@ -0,0 +1,43 @@
+(function() {
+
+	pageLoader.mountPage("/info", function(wnd) {
+
+		var subpath = pageLoader.getSubPath();
+
+		if(subpath == "") {
+			load_page_about(wnd);
+		} else if(subpath == "/tos") {
+			load_page_tos(wnd);
+		} else if(subpath == "/contact") {
+			load_page_contact(wnd);
+		} else if(subpath == "/help") {
+			load_page_help(wnd);
+		} else pageLoader.loadHandler(404);
+
+	});
+
+	var load_page_about = function(wnd) {
+		pageLoader.getTemplate("info/about", function(wnd) {
+			pageLoader.notifyDone();
+		});
+	};
+
+	var load_page_tos = function(wnd) {
+		pageLoader.getTemplate("info/tos", function(wnd) {
+			pageLoader.notifyDone();
+		});
+	};
+
+	var load_page_contact = function(wnd) {
+		pageLoader.getTemplate("info/contact", function(wnd) {
+			pageLoader.notifyDone();
+		});
+	};
+
+	var load_page_help = function(wnd) {
+		pageLoader.getTemplate("info/help", function(wnd) {
+			pageLoader.notifyDone();
+		});
+	};
+
+})();
