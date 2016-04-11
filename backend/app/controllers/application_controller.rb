83cbeb945ce85a3b7ab901dfbbb5e4da032691a3
Anish Goyal <anishgoyal@outlook.com>
2016-04-11 18:07:38 +0000

    Remove fire from stopwords, remove cross origin request disabler

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,15 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   
-  before_filter :disable_cors
-  
-  def disable_cors
-	headers['Access-Control-Allow-Origin'] = '*'
-	headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-	headers['Access-Control-Request-Method'] = '*'
-	headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
-
   def reload_service
   	ApplicationService.reload params[:service_name].to_sym
   	render text:"Reloaded #{params[:service_name]}"
