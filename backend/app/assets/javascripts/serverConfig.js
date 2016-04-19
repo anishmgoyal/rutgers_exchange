c43f5fd01986ab7c671e0fd7a3c3ee3a355b54a0
Anish Goyal <anishgoyal@outlook.com>
2016-04-19 15:54:59 +0000

    Add note to server config

--- backend/app/assets/javascripts/serverConfig.js
+++ backend/app/assets/javascripts/serverConfig.js
@@ -3,10 +3,11 @@ window.server = {
 	configuration: "development",
 	version: "1.0a",
 	serviceName: "Rutgers Exchange"
+	supportEmail: "support@rutgersxchange.com"
 };
 
+// Deprecation notice: only integrated mode will work for image uploads
 window.servers = {
-	amg_development: "http://192.168.1.152:3000",
 	development: "http://localhost:3000",
 	production: "http://www.rutgersexchange.com",
 	integrated: ""
