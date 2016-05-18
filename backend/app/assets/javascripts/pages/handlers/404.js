f42f757fbf6fc0f88e16812c054bc42128877078
Anish Goyal <anishgoyal@outlook.com>
2016-05-17 23:10:39 -0400

    Change the 404 handler to be completely in place and handled with javascript

--- backend/app/assets/javascripts/pages/handlers/404.js
+++ backend/app/assets/javascripts/pages/handlers/404.js
@@ -1,19 +1,13 @@
 $(document).ready(function() {
 
 	pageLoader.mountHandler(404, function(wnd) {
-		$.ajax({
-			url: 'pages/handlers/404.htm',
-			cache: false,
-			datatype: 'html',
-			success: function(data) {
-				wnd.html(data);
-				pageLoader.notifyDone();
-			},
-			error: function() {
-				// Uh oh, this IS our 404 handler...
-				wnd.html("We can't serve that request right now; sorry... Please click <a href='#!/index'>here</a> to go back to the homepage.");
-			}
-		});
+		messageTool.loadMessage(wnd, "Not Found",
+			"Uhhh... that's not here. If you got here by clicking something on this site, that's bad. It's possible " +
+			"whatever happened fixed itself, so try going back. If you get here again, please send us an email about what " +
+			" got you here at " + window.server.supportEmail + ", and we'll try to figure out what the problem is.<br /><br />" +
+			"In the mean time, you can click <a href='#!/index'>here</a> to go back to the home page."
+		);
+		pageLoader.notifyDone();
 	});
 
 });
