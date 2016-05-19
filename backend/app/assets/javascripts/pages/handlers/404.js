c20540026ebc6ba229faa3215b89d5b400479e0f
Anish Goyal <anishgoyal@outlook.com>
2016-05-19 00:46:46 +0000

    Wording change for 404 handler

--- backend/app/assets/javascripts/pages/handlers/404.js
+++ backend/app/assets/javascripts/pages/handlers/404.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	pageLoader.mountHandler(404, function(wnd) {
 		messageTool.loadMessage(wnd, "Not Found",
 			"Uhhh... that's not here. If you got here by clicking something on this site, that's bad. It's possible " +
-			"whatever happened fixed itself, so try going back. If you get here again, please send us an email about what " +
+			"whatever happened fixed itself, so try going back. If you end up here again, please send us an email about what " +
 			" got you here at " + window.server.supportEmail + ", and we'll try to figure out why you're seeing this.<br /><br />" +
 			"In the mean time, you can click <a href='#!/index'>here</a> to go back to the home page."
 		);
